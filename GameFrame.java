61d46ac23cd021c7e00c062b7aaa13f9a8272e94
Atharva Veer <64340527+veeratharva6@users.noreply.github.com>
2022-02-23 01:37:42 -0800

    Add files via upload

---
+++ GameFrame.java
@@ -0,0 +1,26 @@
+
+import java.awt.*;
+import java.awt.event.*;
+
+import java.util.*;
+import javax.swing.*;
+
+
+public class GameFrame extends JFrame{
+
+    GamePanel panel = new GamePanel();
+
+    GameFrame(){
+        panel = new GamePanel();
+        this.add(panel);
+        this.setTitle("Ping Pong");
+        this.setResizable(false);
+        this.setBackground(Color.black);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+
+    }
+
+}
